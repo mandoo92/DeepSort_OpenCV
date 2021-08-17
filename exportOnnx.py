998511d97711b67272bfc173aca31906052e51ef
ManKi Kim <mandoo92@gmail.com>
2021-08-17 15:58:41 +0900

    Remove Dynamic Input for OpenCV DNN

--- exportOnnx.py
+++ exportOnnx.py
@@ -34,9 +34,8 @@ if __name__ == '__main__':
     input_names = ['input']
     output_names = ['output']
 
-    input_tensor = torch.randn(1, 3, 128, 64, device=device)
+    input_tensor = torch.randn(32, 3, 128, 64, device=device)
 
     torch.onnx.export(model.extractor.net.cuda(), input_tensor, output_onnx, export_params=True, verbose=False,
                       input_names=input_names, output_names=output_names, opset_version=10,
-                      do_constant_folding=True,
-                      dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}})
+                      do_constant_folding=True)
